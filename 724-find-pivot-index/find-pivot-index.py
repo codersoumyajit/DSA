7eef0df89ea1c015a6e76bb76c9ec4962f0576da
Soumyajit Paul <161820220+codersoumyajit@users.noreply.github.com>
2026-07-18 10:22:32 +0530

    Time: 3 ms (90.03%) | Memory: 20.3 MB (81.86%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -3,7 +3,6 @@ class Solution:
         leftsum = 0
         total = sum(nums)
         for i in range(len(nums)):
-            
             rightsum = total - leftsum - nums[i]
             if rightsum == leftsum:
                 return i
