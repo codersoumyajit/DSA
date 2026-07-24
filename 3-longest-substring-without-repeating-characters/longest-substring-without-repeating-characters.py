3096748dc243019d7de3967c2d5fa801974ad8b8
Soumyajit Paul <161820220+codersoumyajit@users.noreply.github.com>
2026-07-24 10:13:26 +0530

    Time: 319 ms (5.04%) | Memory: 19.2 MB (68.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,9 +3,10 @@ class Solution:
         longest = 0
         for i in range(len(s)):
             seen = set()
-            for j in range(i, len(s)):
+            for j in range(i,len(s)):
                 if s[j] in seen:
                     break
+                
                 seen.add(s[j])
-                longest = max(longest, j -i +1)
-        return longest   
+                longest = max(longest, j-i+1)
+        return longest
